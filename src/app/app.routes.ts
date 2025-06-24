8d73d3ca8f0d6dc3ec892c709bae477bbc5e31eb
uidhtml <54753484+uidhtml@users.noreply.github.com>
2025-06-24 12:23:46 +0530

    Added resource API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ResourceApiComponent } from './resource-api/resource-api.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'resourceApi', component: ResourceApiComponent },
+  { path: '', redirectTo: '/resourceApi', pathMatch: 'full' },
+];
