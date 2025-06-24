b16941ba680ce1466df06598890539ebb333c897
uidhtml <54753484+uidhtml@users.noreply.github.com>
2025-06-24 21:22:03 +0530

    Added Http Resource API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ResourceApiComponent } from './resource-api/resource-api.component';
+import { HttpResourceApiComponent } from './http-resource-api/http-resource-api.component';
 
 export const routes: Routes = [
+  { path: 'httpResourceApi', component: HttpResourceApiComponent },
   { path: 'resourceApi', component: ResourceApiComponent },
-  { path: '', redirectTo: '/resourceApi', pathMatch: 'full' },
+  { path: '', redirectTo: '/httpResourceApi', pathMatch: 'full' },
 ];
